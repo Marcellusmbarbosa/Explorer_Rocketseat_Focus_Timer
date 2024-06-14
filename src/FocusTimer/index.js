30028ec9870b03c3d0263dc02655d53de1df4df4
marcellusmbarbosa <marcellusmbarbosa@gmail.com>
2024-06-13 22:57:36 -0300

    Dia finalizado

--- src/FocusTimer/index.js
+++ src/FocusTimer/index.js
@@ -1,8 +1,8 @@
 import state from './state.js'
-
+import * as events from './events.js'
 export function start(minutes, seconds) {
   state.minutes = minutes
   state.seconds = seconds
 
-  console.log(state)
+events.registerControls()
 }
